c7d131aae2f0acf5ee4535af66cae14b2d119be7
xongeeuse <xongeeuse@gmail.com>
2024-09-20 10:58:22 +0900

    240920

--- 04.django/inlesson/03-model/articles/admin.py
+++ 04.django/inlesson/03-model/articles/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+# `admin` `site`에 `등록`한다! 순서로 기억하기!
+admin.site.register(Article)
--- 04.django/inlesson/03-model/articles/models.py
+++ 04.django/inlesson/03-model/articles/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# ()에 들어가는 요소는 상위 클래스, 상속의 의미
+# 위에서 import한 models모듈의 Model 클래스 상속 받겠어!
+class Article(models.Model):
+    # CharField 이름에서 클래스라는 것을 알 수 있음!
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
