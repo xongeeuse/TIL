c71833328dd05c22eefe07e1d4717602955053e7
xongeeuse <xongeeuse@gmail.com>
2024-09-24 09:09:04 +0900

    240924 django05 update

---
+++ 04.django/inlesson/05-orm-with-view/articles/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+# 모델 클래스 가져오기
+from .models import Article
+
+# Create your views here.
+def index(request):
+    # 게시글 전체 조회 요청 to DB
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
