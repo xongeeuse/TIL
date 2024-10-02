34024a872a935415810e383dd5efd0a7f99c295b
xongeeuse <xongeeuse@gmail.com>
2024-10-02 15:22:35 +0900

    241002 upload onsil_hw

---
+++ 04.django/01.onsil/Django_09/django_hw_9_2/accounts/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
+from .models import User
+
+from django.contrib.auth import login as auth_login
+
+# Create your views here.
+def index(request):
+    users = User.objects.all()
+    context = {
+        'users' : users,
+    }
+    return render(request, 'accounts/index.html', context)
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('accounts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/login.html', context)
---
+++ 04.django/01.onsil/Django_09/django_hw_9_4/accounts/views.py
@@ -0,0 +1,82 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import get_user_model, update_session_auth_hash
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.views.decorators.http import require_http_methods, require_POST
+
+
+User = get_user_model()
+
+# Create your views here.
+def index(request):
+    persons = User.objects.all()
+    context = {
+        'persons': persons
+    }
+    return render(request, 'accounts/index.html', context)
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('accounts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+@require_POST
+def logout(request):
+    # if request.method == "POST":
+    auth_logout(request)
+    return redirect('accounts:index')
+
+
+def update(request):
+    if request.method == "POST":
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:index')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+
+def change_password(request, user_pk):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('accounts:index')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/change_password.html', context)
