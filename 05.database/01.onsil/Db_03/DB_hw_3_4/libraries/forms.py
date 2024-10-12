5c88b08072d9ee987ef258c94ece9d876ae58582
xongeeuse <xongeeuse@gmail.com>
2024-10-12 20:33:52 +0900

    241012

--- 05.database/01.onsil/Db_03/DB_hw_3_4/libraries/forms.py
+++ 05.database/01.onsil/Db_03/DB_hw_3_4/libraries/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-from .models import Author, Book
+from .models import Book
 
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         exclude = ('author',)
+
