e0a446676cc65a291633aaf8fa8e4f6c34ad7f23
xongeeuse <xongeeuse@gmail.com>
2024-10-30 11:15:35 +0900

    241030 JS with Django

---
+++ 06.javascript/00.inlesson/08-ajax-with-django/README.md
@@ -0,0 +1,15 @@
+## fixtures 및 계정 정보
+
+- fixtures 데이터 load
+
+  ```bash
+  $ python manage.py migrate
+  $ python manage.py loaddata articles.json comments.json users.json
+  ```
+
+
+- 계정 정보
+    | username | password    |
+    | -------- | ----------- |
+    | user1    | `ssafy1212` |
+    | user2    | `ssafy1212` |
---
+++ 06.javascript/00.inlesson/08-ajax-with-django/accounts/views.py
@@ -0,0 +1,124 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+# Create your views here.
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+
+@login_required
+def logout(request):
+    auth_logout(request)
+    return redirect('articles:index')
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('articles:index')
+
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+@login_required
+def delete(request):
+    request.user.delete()
+    return redirect('articles:index')
+
+
+@login_required
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('articles:index')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+
+@login_required
+def change_password(request, user_pk):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('articles:index')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/change_password.html', context)
+
+
+from django.contrib.auth import get_user_model
+
+
+def profile(request, username):
+    User = get_user_model()
+    person = User.objects.get(username=username)
+    context = {
+        'person': person,
+    }
+    return render(request, 'accounts/profile.html', context)
+
+from django.http import JsonResponse
+
+def follow(request, user_pk):
+    User = get_user_model()
+    you = User.objects.get(pk=user_pk)
+    me = request.user
+
+    if me != you:
+        # 9. JS에게 팔로우 상태 여부를 전달할 데이터 작성
+        if me in you.followers.all():
+            you.followers.remove(me)
+            # me.followings.remove(you)
+            is_followed = False
+        else:
+            you.followers.add(me)
+            # me.followings.add(you)
+            is_followed = True
+        context = {
+            # 14. 팔로워 수와 팔로잉 수에 대한 데이터 작성
+            'is_followed': is_followed,
+            'followings_count': you.followings.count(),
+            'followers_count': you.followers.count(),
+        }
+        # 10. JSON 데이터로 응답(위 import 참고)
+        return JsonResponse(context)
+    return redirect('accounts:profile', you.username)
