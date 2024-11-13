2afff5a0ff42862f9b8cfab23440a76711a72cf6
xongeeuse <xongeeuse@gmail.com>
2024-11-13 09:04:12 +0900

    241113 Vue with DRF

---
+++ 07.vue/00.inlesson/07-01-vue-with-drf/vue-project/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <nav>
+    </nav>
+  </header>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+</style>
---
+++ 07.vue/00.inlesson/07-01-vue-with-drf/vue-project/src/components/ArticleList.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h3>Article List</h3>
+    <ArticleListItem />
+  </div>
+</template>
+
+<script setup>
+import ArticleListItem from '@/components/ArticleListItem.vue'
+</script>
