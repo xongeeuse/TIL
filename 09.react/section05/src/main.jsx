278f8dbecf450f1bc16422d7faad1a74ed4a8256
Jiyoung Song <xongeeuse@gmail.com>
2025-01-13 09:33:54 +0900

    250113

--- 09.react/section05/src/main.jsx
+++ 09.react/section05/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 // const Hello = () => {
-//     return <div>hello</div>
+//     return <div>hello!</div>
 // }
 
 createRoot(document.getElementById('root')).render(
